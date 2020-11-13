fa541e72724c49e2e7aaf02b2d132e4650393f35
Maikol Chow Wang <whysoraccoony@gmail.com>
2020-11-13 12:30:25 -0800

    added a navbar to login

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+function readQuote(){
+    db.collection("quotes").doc("01")
+    .onSnapshot(function(snap){
+        console.log(snap.data());   //print the document fields of "01"
+        console.log(snap.data().message);
+        document.getElementById("abc").innerText = snap.data().message;
+    })
+}
+readQuote();
